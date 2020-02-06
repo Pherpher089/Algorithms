2c3bac3c55ec1bd68d4cdc66a48322e61c491a82
Pherpher089 <Christutor089@gmail.com>
2020-02-05 20:11:12 -0800

    Finished the recipee ingredients

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,33 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    _recipe = list(recipe.values())
+    _ingredients = list(ingredients.values())
+    print(f"_recipe: {_recipe}")
+    print(f"_ingredients: {_ingredients}")
+    if(len(_ingredients) < len(_recipe)):
+        return 0
+
+    batch_counts = [0] * len(_recipe)
+
+    for i in range(len(_recipe)):
+        batch_counts[i] = _ingredients[i] // _recipe[i]
+
+    print(f"batch_counts: {batch_counts}")
+    min_batch_count = batch_counts[0]
+
+    for i in range(1, len(_recipe)):
+        if batch_counts[i] < min_batch_count:
+            min_batch_count = batch_counts[i]
+    return min_batch_count
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    _recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    _ingredients = {'milk': 132, 'butter': 480, 'flour': 15}
+    print("{batches} batches can be made from the available _ingredients: {_ingredients}.".format(
+        batches=recipe_batches(_recipe, _ingredients), _ingredients=_ingredients))
