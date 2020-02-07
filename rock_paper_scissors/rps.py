0aca21d390d87051d49bf26fd73eb7646d11f25c
Christopher Tutor <Christutor089@gmail.com>
2020-02-07 13:11:39 -0800

    Finished Rock Paper Scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    plays = ['rock', 'paper', 'scisors']
+    pos_plays = [None] * (n^3)
+    
+    def rps(game, game_number):
+        for i in range(len(plays)):
+            game.append(plays[i])
+            if game_number == n:
+                pos_plays.append([i for i in game])
+            else:
+                rps(game, game_number + 1)
+            game.pop()
+    
+    rps([], 1)
+    return pos_plays
+    
+print(rock_paper_scissors(6))
 
 
 if __name__ == "__main__":
